27c96febef6ecfa4bd2647ec63943d411e03db0d
Justin Le <justin@jle.im>
2016-11-23 01:54:16 -0800

    documenting methods

--- src/Numeric/Hamilton.hs
+++ src/Numeric/Hamilton.hs
@@ -7,14 +7,15 @@
 {-# LANGUAGE TypeInType          #-}
 
 module Numeric.Hamilton
-  ( Config(..)
+  ( System
+  , Config(..)
   , Phase(..)
-  , System(..)
   , mkSystem
   , mkPhase
+  , fromPhase
   , momenta
   , velocities
-  , ke
+  , keC
   , keP
   , lagrangian
   , hamiltonian
@@ -31,19 +32,69 @@ import qualified Control.Comonad.Cofree       as C
 import qualified Data.Vector.Generic.Sized    as VG
 import qualified Data.Vector.Sized            as V
 
+-- | Represents the full state of a system of @n@ generalized coordinates
+-- in configuration space (informally, "positions and velocities")
+--
+-- A configuration space representaiton is more directly "physically
+-- meaningful" and intuitive/understandable to humans than a phase space
+-- representation.  However, it's much less mathematically ideal to work
+-- with because of the lack of some neat underlying symmetries.
+--
+-- You can convert a @'Config' n@ into a @'Phase' n@ (convert from
+-- configuration space to phase space) for a given system with 'mkPhase'.
+-- This allows you to state your system in configuration space and then
+-- convert it to phase space before handing it off to the hamiltonian
+-- machinery.
 data Config :: Nat -> Type where
-    Cfg :: { cfgPos :: !(R n)
+    Cfg :: { -- | The current values ("positions") of each of the @n@
+             -- generalized coordinates
+             cfgPos :: !(R n)
+             -- | The current rate of changes ("velocities") of each of the
+             -- @n@ generalized coordinates
            , cfgVel :: !(R n)
            }
         -> Config n
 
+-- | Represents the full state of a system of @n@ generalized coordinates
+-- in phase space (informally, "positions and momentums").
+--
+-- Phase space representations are much nicer to work with mathematically
+-- because of some neat underlying symmetries.  For one, positions and
+-- momentums are "interchangeable" in a system; if you swap every
+-- coordinate's positions with their momentums, and also swap them in the
+-- equations of motions, you get the same system back.  This isn't the case
+-- with configuration space representations.
+--
+-- A hamiltonian simulation basically describes the trajectory of each
+-- coordinate through phase space, so this is the /state/ of the
+-- simulation.  However, configuration space representations are much more
+-- understandable to humans, so it might be useful to give an initial state
+-- in configuration space using 'Config', and then convert it to a 'Phase'
+-- with 'mkPhase'.
 data Phase :: Nat -> Type where
-    Phs :: { phsPos :: !(R n)
+    Phs :: { -- | The current values ("positions") of each of the @n@
+             -- generalized coordinates.
+             phsPos :: !(R n)
+             -- | The current conjugate momenta ("momentums") to each of
+             -- the @n@ generalized coordinates
            , phsMom :: !(R n)
            }
         -> Phase n
 
 -- TODO: cache inverses, JMJs?
+
+-- | Represents a physical system in which physics happens.  A @'System'
+-- m n@ is a system whose state described using @n@ generalized coordinates
+-- (an "@n@-dimensional" system), where the underlying cartesian coordinate
+-- space is @m@-dimensional.
+--
+-- For the most part, you are supposed to be able to ignore @m@.  @m@ is
+-- only provided because it's useful when plotting/drawing the system with
+-- a given state back in rectangular coordinates.
+--
+-- A @'System' m n@'s state is described using a @'Config' n@ (which
+-- describes the system in configuration space) or a @'Phase' n@ (which
+-- describes the system in phase space).
 data System :: Nat -> Nat -> Type where
     Sys :: { sysMass          :: R m
            , sysCoords        :: R n -> R m
@@ -68,11 +119,28 @@ vec2l
     -> L m n
 vec2l = fromJust . (\rs -> withRows rs exactDims) . toList . fmap vec2r
 
+-- | Create a system with @n@ generalized coordinates by providing the
+-- underlying inertials, describing its coordinate space, and giving
+-- pontential energy function.
+--
+-- Given by describing some fundamental properties of the underlying
+-- cartesian coordinate space of the system (the inertia of each coordinate
+-- and the potential energy function), along with a conversion function
+-- from the generalized coordinate space to the underlying cartesian
+-- coordinate space.
 mkSystem
     :: forall m n. (KnownNat m, KnownNat n)
-    => R m
+    => R m      -- ^ The "inertia" of each of the @m@ coordinates
+                -- in the underlying cartesian space of the system.  This
+                -- should be mass for linear coordinates and rotational
+                -- inertia for angular coordinates.
     -> (forall a. RealFloat a => V.Vector n a -> V.Vector m a)
+                -- ^ Conversion function to convert points in the
+                -- generalized coordinate space to the underlying cartesian
+                -- space of the system.
     -> (forall a. RealFloat a => V.Vector m a -> a)
+                -- ^ The potential energy of the system as a function of
+                -- points in the underlying cartesian space of the system.
     -> System m n
 mkSystem m f u = Sys m
                      (vec2r . f . r2vec)
@@ -89,6 +157,9 @@ mkSystem m f u = Sys m
         -> V.Vector n (V.Vector n Double)
     j2 = fmap (fmap C.extract . C.unwrap) . C.unwrap
 
+-- | Compute the generalized momenta conjugate to each generalized
+-- coordinate of a system by giving the configuration-space state of the
+-- system.
 momenta
     :: (KnownNat m, KnownNat n)
     => System m n
@@ -98,6 +169,14 @@ momenta Sys{..} Cfg{..} = tr j #> diag sysMass #> j #> cfgVel
   where
     j = sysJacobian cfgPos
 
+-- | Convert a configuration-space representaiton of the state of the
+-- system to a phase-space representation.
+--
+-- Useful because the hamiltonian simulations use 'Phase' as its working
+-- state, but 'Config' is a much more human-understandable and intuitive
+-- representation.  This allows you to state your starting state in
+-- configuration space and convert to phase space for your simulation to
+-- use.
 mkPhase
     :: (KnownNat m, KnownNat n)
     => System m n
@@ -105,25 +184,32 @@ mkPhase
     -> Phase n
 mkPhase s = Phs <$> cfgPos <*> momenta s
 
-ke  :: (KnownNat m, KnownNat n)
+-- | The kinetic energy of a system, given the system's state in
+-- configuration space.
+keC :: (KnownNat m, KnownNat n)
     => System m n
     -> Config n
     -> Double
-ke s = do
+keC s = do
     vs <- cfgVel
     ps <- momenta s
     return $ (vs <.> ps) / 2
 
+-- | The Lagrangian of a system (the difference between the kinetic energy
+-- and the potential energy), given the system's state in configuration
+-- space.
 lagrangian
     :: (KnownNat m, KnownNat n)
     => System m n
     -> Config n
     -> Double
 lagrangian s = do
-    t <- ke s
+    t <- keC s
     u <- sysPotential s . sysCoords s . cfgPos
     return (t - u)
 
+-- | Compute the rate of change of each generalized coordinate by giving
+-- the state of the system in phase space.
 velocities
     :: (KnownNat m, KnownNat n)
     => System m n
@@ -134,6 +220,21 @@ velocities Sys{..} Phs{..} = inv jmj #> phsMom
     j   = sysJacobian phsPos
     jmj = tr j <> diag sysMass <> j
 
+-- | Invert 'mkPhase' and convert a description of a system's state in
+-- phase space to a description of the system's state in configuration
+-- space.
+--
+-- Possibly useful for showing the phase space representation of a system's
+-- state in a more human-readable/human-understandable way.
+fromPhase
+    :: (KnownNat m, KnownNat n)
+    => System m n
+    -> Phase n
+    -> Config n
+fromPhase s = Cfg <$> phsPos <*> velocities s
+
+-- | The kinetic energy of a system, given the system's state in
+-- phase space.
 keP :: (KnownNat m, KnownNat n)
     => System m n
     -> Phase n
@@ -143,6 +244,8 @@ keP s = do
     vs <- velocities s
     return $ (vs <.> ps) / 2
 
+-- | The Hamiltonian of a system (the sum of kinetic energy and the
+-- potential energy), given the system's state in phase space.
 hamiltonian
     :: (KnownNat m, KnownNat n)
     => System m n
